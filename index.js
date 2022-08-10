68cb931754ed50954852ac895e1346e50534b93c
MAGHC <windowsapple95@gmail.com>
2022-08-10 16:56:16 +0900

    1차 완성

--- index.js
+++ index.js
@@ -3,22 +3,40 @@
 const playButton = document.querySelector(".play__button");
 const playTime = document.querySelector(".play__time--content");
 const playScore = document.querySelector(".play__score--content");
+const playButton2 = document.querySelector(".play__button--stop");
+const playButton1 = document.querySelector(".play__button--shpae");
 
 const mainSection = document.querySelector(".section-main");
 const mainRect = mainSection.getBoundingClientRect();
 
 let time = 9;
-let score = 10;
+let score = 5;
+let startStatus = false;
 
 const CARROT_SIZE = 80;
 
 const initGame = () => {
+  mainSection.innerHTML = "";
   addItem("carrot", 5, "img/carrot.png");
   addItem("bug", 5, "img/bug.png");
+  startStatus = !startStatus;
 };
 
 const randomNum = (min, max) => {
   return Math.floor(Math.random() * (max - min) + min);
+};
+
+const buttonViewToggle = () => {
+  playButton2.classList.toggle("none");
+  playButton1.classList.toggle("none");
+};
+
+const carrotSound = new Audio("./sound/carrot_pull.mp3");
+const bugound = new Audio("./sound/bug_pull.mp3");
+const bgm = new Audio("./sound/bg.mp3");
+
+const playSound = (source) => {
+  source.play();
 };
 
 const addItem = (classname, number, imgpath) => {
@@ -37,21 +55,46 @@ const addItem = (classname, number, imgpath) => {
     img.style.left = `${x}px`;
     img.style.top = `${y}px`;
     mainSection.appendChild(img);
-    console.log(x);
   }
 };
 
 window.addEventListener("load", () => {
-  const startTimer = setInterval(() => {
-    playTime.innerText = `00:${time--}`;
-    if (time < 0) {
-      clearInterval(startTimer);
+  playButton.addEventListener("click", () => {
+    if (!startStatus) {
+      playSound(bgm);
+      buttonViewToggle();
+      initGame();
+      const startTimer = setInterval(() => {
+        playTime.innerText = `00:${time--}`;
+        if (time === 0 && score !== 0) {
+          playSound(bugound);
+          alert("패배");
+          location.reload();
+        } else if (time > 0 && score === 0) {
+          alert("win");
+          clearInterval(startTimer);
+          location.reload();
+        }
+      }, 1000);
+    } else {
+      location.reload();
     }
-  }, 1000);
+  });
 
-  initGame();
-
-  playScore.innerText = score;
+  mainSection.addEventListener("click", (e) => {
+    const target = e.target;
+    if (!startStatus) {
+      return;
+    }
+    if (target.matches(".carrot")) {
+      target.classList.toggle("none");
+      --score;
+      playScore.innerText = score;
+      playSound(carrotSound);
+    } else if (target.matches(".bug")) {
+      playSound(bugound);
+      alert("패배");
+      location.reload();
+    }
+  });
 });
-
-console.log(mainSection);
